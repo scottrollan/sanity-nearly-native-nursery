b38bed762f8df6157d536757722c025859df009c
Nathan Sierra <arreisnahtan@gmail.com>
2019-11-03 17:18:44 -0500

    reverting

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -8,23 +8,21 @@ import schemaTypes from 'all:part:@sanity/base/schema-type';
 import page from './documents/page';
 import route from './documents/route';
 import siteConfig from './documents/siteConfig';
-import plant from './documents/plant';
 
 // Object types
-import cta from './objects/cta'
-import embedHTML from './objects/embedHTML'
-import figure from './objects/figure'
-import internalLink from './objects/internalLink'
-import link from './objects/link'
-import portableText from './objects/portableText'
-import simplePortableText from './objects/simplePortableText'
-import amount from './objects/amount'
+import cta from './objects/cta';
+import embedHTML from './objects/embedHTML';
+import figure from './objects/figure';
+import internalLink from './objects/internalLink';
+import link from './objects/link';
+import portableText from './objects/portableText';
+import simplePortableText from './objects/simplePortableText';
 
 // Landing page sections
-import hero from './objects/hero'
-import imageSection from './objects/imageSection'
-import mailchimp from './objects/mailchimp'
-import textSection from './objects/textSection'
+import hero from './objects/hero';
+import imageSection from './objects/imageSection';
+import mailchimp from './objects/mailchimp';
+import textSection from './objects/textSection';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -46,7 +44,5 @@ export default createSchema({
     simplePortableText,
     siteConfig,
     textSection,
-    plant,
-    amount
-  ])
-})
+  ]),
+});
